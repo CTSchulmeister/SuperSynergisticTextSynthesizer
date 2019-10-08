76b04b6093b99ff783223488847bc449ff5aabba
CTSchulmeister <CTSchulmeister@gmail.com>
2019-10-08 17:03:46 -0400

    Change Controls.js to a functional component

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Controls extends Component {
-    render() {
-        let alert = null;
+const Controls = (props) => {
+    let alert = null;
 
-        if(!this.props.isValidNumber) {
-            alert = (
-                <div className="ssts-controls__alert">
-                    The number of paragraphs must be a number between 1 and 30.
-                </div>
-            );
-        } 
-
-        return (
-            <div className="ssts-controls">
-                Controls
-                { alert }
-                <form className="ssts-controls__form" onSubmit={ this.props.handleSubmit }>
-                    <label className="ssts-controls__label" htmlFor="numberOfParagraphs">Number of Paragraphs</label>
-                    <input 
-                        className="sstc-controls__text-field"
-                        value={ this.props.numberOfParagraphs } 
-                        onChange={ this.props.changeNumberOfParagraphs }
-                        onBlur={ this.props.validateNumberOfParagraphs }
-                        name="numberOfParagraphs"
-                    />
-                    <button className="ssts-controls__submit-button" type="submit">Generate Text</button>
-                </form>
+    if(!props.isValidNumber) {
+        alert = (
+            <div className="ssts-controls__alert">
+                The number of paragraphs must be a number between 1 and 30.
             </div>
         );
-        
-    }
+    } 
+
+    return (
+        <div className="ssts-controls">
+            Controls
+            { alert }
+            <form className="ssts-controls__form" onSubmit={ props.handleSubmit }>
+                <label className="ssts-controls__label" htmlFor="numberOfParagraphs">Number of Paragraphs</label>
+                <input 
+                    className="sstc-controls__text-field"
+                    value={ props.numberOfParagraphs } 
+                    onChange={ props.changeNumberOfParagraphs }
+                    onBlur={ props.validateNumberOfParagraphs }
+                    name="numberOfParagraphs"
+                />
+                <button className="ssts-controls__submit-button" type="submit">Generate Text</button>
+            </form>
+        </div>
+    );
 }
 
 export default Controls;
