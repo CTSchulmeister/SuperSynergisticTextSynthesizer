26bba2b053269f3fc4f337b354f33d48aaa16411
CTSchulmeister <CTSchulmeister@gmail.com>
2019-10-09 17:21:51 -0400

    Updated README

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const Header = (props) => {
     return (
         <header className="ssts-header">
             <h1 className="ssts-header__app-name">Super Synergistic Text Synthesizer</h1>
+            <span className="ssts-header__author-name">By Christian Schulmeister</span>
         </header>
     );
 }
